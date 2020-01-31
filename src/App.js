edba3faa0ca53bee8056822588e32041d8dffc55
dfcastanedat <dfcastanedat@unal.edu.co>
2020-01-31 10:24:34 -0500

    Header 1.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-
+import {Navbar} from "react-bootstrap";
+import logo from "./logo.svg";
+import "./App.css"
 function App() {
   return (
       <div>
-          <h1>porongas</h1>
+          <Navbar className="header">
+              <Navbar.Brand className="header-name" href="#home">
+                  <img
+                      alt="Logo"
+                      src={logo}
+                      width="50"
+                      height="50"
+                      className="d-inline-block align-top"
+                  />{' '}
+                  Next House
+              </Navbar.Brand>
+          </Navbar>
       </div>
   );
 }
